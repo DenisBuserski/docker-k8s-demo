681c82491573395f0202a0e0bf0dfd9f2a4c90fa
DenisKP <denis.buserski@gmail.com>
2024-07-04 21:34:28 +0300

    demo-docker-with-java
    Simple "Hello Docker" app, to see how Dockerfile works.

---
+++ demo-docker-with-java/src/main/java/com/demo/Main.java
@@ -0,0 +1,17 @@
+package com.demo;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Docker!");
+
+        int count = 0;
+        try {
+            while (true) {
+                Thread.sleep(2*1000);
+                System.out.println("I'm still here! Iteration " + count++);
+            }
+        } catch (InterruptedException exception) {
+            exception.printStackTrace();
+        }
+    }
+}
